6b01e413f4f6b07c4ae2f9a42a2602d3aace84c2
hassansabbir <mahmoodsabbir3087@gmail.com>
2023-09-05 11:33:50 +0600

    services section added

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,6 +1,6 @@
 const Banner = () => {
   return (
-    <div className="pt-[217px] h-[900px                ] ps-[80px]">
+    <div className="pt-[217px] h-[900px] border-b-2 mb-14 ps-[80px]">
       <p className="text-xs font-bold w-[220px]">
         <span className="text-[#0078FF]">$100M+</span> revenue Generated
       </p>
@@ -13,12 +13,25 @@ const Banner = () => {
         with our out-of-the-box marketing skills! Since we know every second is
         valuable, we strive for quick delivery of results.
       </p>
-      <div className="flex gap-4">
-        <button className="w-[216px] px-6 py-[10px] rounded-[32px] border bg-[#0078FF] font-bold text-white">
+      <div className="flex mt-[44px] gap-4">
+        <button className="w-[216px] px-6 py-[10px] rounded-[32px] bg-[#0078FF] active:bg-[#003a7c] hover:bg-[#0157b9] border border-[#0078FF] font-bold text-white">
           Get free Consultation
         </button>
-        <button className="w-[216px] px-6 py-[10px] rounded-[32px] border border-black font-bold">
-          Explore Services
+        <button className="w-[216px] flex gap-2 justify-center items-center btn-outline px-6  py-[10px] rounded-[32px] border border-black font-bold">
+          Explore Services{" "}
+          <svg
+            className="hover:text-white"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+          >
+            <path
+              d="M11 2.04938V4.06189C7.05369 4.55399 4 7.92038 4 12C4 16.4183 7.58172 20 12 20C16.0796 20 19.446 16.9463 19.9381 13H21.9506C21.4489 18.0533 17.1853 22 12 22C6.47715 22 2 17.5228 2 12C2 6.81465 5.94668 2.5511 11 2.04938ZM18.7071 6.70711L12 13.4142L10.5858 12L17.2929 5.29289L14 2H22V10L18.7071 6.70711Z"
+              fill="#14171A"
+            />
+          </svg>
         </button>
       </div>
     </div>
--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -1,9 +1,78 @@
 import rainbowAppLogo from "../../../assets/rainbow-app-icon.png";
+import brandingIcon from "../../../assets/BrandingIcon.png";
+// import developmentIcon from "../../../assets/DevlopmentIcon.png";
+// import marketingIcon from "../../../assets/MarketingIcon.png";
 
 const Services = () => {
   return (
-    <div>
-      <img src={rainbowAppLogo} className="w-[42px] h-[42px]" alt="" />
+    <div className="px-20 mb-10">
+      <div>
+        <img src={rainbowAppLogo} className="w-[42px] h-[42px]" alt="" />
+        <h2 className="text-[32px] font-bold">Our Services</h2>
+        <p className="w-[579px] text-lg">
+          Striving for better business with convenience, we create simple yet
+          effective and powerful plans tailored for you; We offer:
+        </p>
+      </div>
+
+      {/* fliping cards start from here */}
+
+      <div className="mt-16 flex justify-between">
+        <div className="group h-[252px] w-[372px] bg-[#e9eef5] hover:bg-white hover:shadow-2xl hover:shadow-blue-200 rounded-3xl border border-slate-200 [perspective:1000px]">
+          <div className="relative h-full w-full rounded-xl px-4 py-6 transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)] group-hover:delay-[0.5s] ">
+            <div className="absolute flex flex-col items-center justify-center inset-0">
+              <img src={brandingIcon} alt="" />
+              <h2 className="text-[24px] font-bold">Branding services</h2>
+            </div>
+            <div className="absolute flex flex-col items-center rounded-3xl justify-center inset-0 h-full w-full bg-white px-12 text-center [transform:rotateY(180deg)] [backface-visibility:hidden]">
+              <p className="text-[14px]">
+                We help you establish a unique and recognizable identity through
+                captivating logo design, purpose-driven brand strategy, and
+                compelling messaging.
+              </p>
+              <p className="text-[16px] mt-5 text-[#0078FF] font-bold">
+                View More
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="group h-[252px] w-[372px] bg-[#e2e6eb] hover:bg-white hover:shadow-2xl hover:shadow-blue-200 rounded-3xl border border-slate-200 [perspective:1000px]">
+          <div className="relative h-full w-full rounded-xl px-4 py-6 transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)] group-hover:delay-[0.5s] ">
+            <div className="absolute flex flex-col items-center justify-center inset-0">
+              <img src={brandingIcon} alt="" />
+              <h2 className="text-[24px] font-bold">Branding services</h2>
+            </div>
+            <div className="absolute flex flex-col items-center rounded-3xl justify-center inset-0 h-full w-full bg-white px-12 text-center [transform:rotateY(180deg)] [backface-visibility:hidden]">
+              <p className="text-[14px]">
+                We help you establish a unique and recognizable identity through
+                captivating logo design, purpose-driven brand strategy, and
+                compelling messaging.
+              </p>
+              <p className="text-[16px] mt-5 text-[#0078FF] font-bold">
+                View More
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="group h-[252px] w-[372px] bg-[#e2e6eb] hover:bg-white hover:shadow-2xl hover:shadow-blue-200 rounded-3xl border border-slate-200 [perspective:1000px]">
+          <div className="relative h-full w-full rounded-xl px-4 py-6 transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)] group-hover:delay-[0.5s] ">
+            <div className="absolute flex flex-col items-center justify-center inset-0">
+              <img src={brandingIcon} alt="" />
+              <h2 className="text-[24px] font-bold">Branding services</h2>
+            </div>
+            <div className="absolute flex flex-col items-center rounded-3xl justify-center inset-0 h-full w-full bg-white px-12 text-center [transform:rotateY(180deg)] [backface-visibility:hidden]">
+              <p className="text-[14px]">
+                We help you establish a unique and recognizable identity through
+                captivating logo design, purpose-driven brand strategy, and
+                compelling messaging.
+              </p>
+              <p className="text-[16px] mt-5 text-[#0078FF] font-bold">
+                View More
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
