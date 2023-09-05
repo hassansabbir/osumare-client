d778557ef920a41dbf032ef8801b5e2b52629e9b
hassansabbir <mahmoodsabbir3087@gmail.com>
2023-09-05 12:11:45 +0600

    tab section added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "../Banner/Banner";
 import Services from "../Services/Services";
+import TabSection from "../TabSection/TabSection";
 
 const Home = () => {
   return (
     <div>
       <Banner />
       <Services />
+      <TabSection />
     </div>
   );
 };
---
+++ src/Pages/Home/TabSection/TabSection.jsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import video1 from "../../../assets/osumareVideo1.mp4";
+import awardImg from "../../../assets/awd 1.png";
+
+const TabSection = () => {
+  const [activeTab, setActiveTab] = useState(0);
+
+  const tabs = [
+    {
+      label: "Tab 1",
+      content: (
+        <div>
+          <img
+            className="rounded-3xl   w-full h-[610px]"
+            src={awardImg}
+            alt=""
+          />
+        </div>
+      ),
+    },
+    {
+      label: "Tab 2",
+      content: (
+        <div>
+          <video
+            className="rounded-3xl w-full h-[610px]"
+            controls
+            src={video1}
+          />
+        </div>
+      ),
+    },
+    {
+      label: "Tab 3",
+      content: (
+        <div>
+          <img className="rounded-3xl w-full h-[610px]" src={awardImg} alt="" />
+        </div>
+      ),
+    },
+  ];
+
+  const handleTabClick = (index) => {
+    setActiveTab(index);
+  };
+  return (
+    <div className="flex items-center px-[80px] my-40">
+      <div className="  p-4">
+        <h2 className="text-[32px] font-bold">
+          Know Why Business <br /> Trust Osumare
+        </h2>
+        <ul className="flex my-10 flex-col gap-5">
+          {tabs.map((tab, index) => (
+            <li
+              key={index}
+              onClick={() => handleTabClick(index)}
+              className={`cursor-pointer btn w-[200px] rounded-3xl py-3 ${
+                activeTab === index
+                  ? "bg-blue-500 w-[300px] text-white"
+                  : "hover:bg-gray-300"
+              }`}
+            >
+              {tab.label}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="w-3/4 rounded-3xl p-4">
+        <div className="bg-white rounded-3xl p-4">
+          <div className="rounded-3xl">{tabs[activeTab].content}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TabSection;
